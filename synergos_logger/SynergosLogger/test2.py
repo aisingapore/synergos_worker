a19a29f24fb4fdf9edfa11dd1a7cc849dbce4195
Kelvin Soh <kelvinsoh@Kelvins-MacBook-Pro.local>
2020-11-10 11:35:01 +0800

    clean up synergos_logger

--- synergos_logger/SynergosLogger/test2.py
+++
@@ -1,5 +0,0 @@
-import test1
-import init_logging
-
-logging = init_logging.logging
-logging.info("c")
--- synergos_logger/SynergosLogger/test3.py
+++
@@ -1,6 +0,0 @@
-import test1
-import test2
-import init_logging
-
-logging = init_logging.logging
-logging.info("k")
